2fbd3e889dde139aa34bac718b632356559ea14d
Naz-Z <nkzhcg@umsystem.edu>
2025-05-01 12:48:18 -0500

    Delete sos_game/project/sos_game directory

--- sos_game/project/sos_game/main.py
+++
@@ -1,9 +0,0 @@
-# main.py (in the parent directory)
-from sos_game.ui import SOSGameUI
-
-def main():
-    app = SOSGameUI()
-    app.mainloop()
-
-if __name__ == "__main__":
-    main()
--- sos_game/project/sos_game/model/game_mode.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class GameMode(Enum):
-    """Enumeration for the two game modes"""
-    SIMPLE = 1
-    GENERAL = 2
